c8f562e9fbc8ac0106c26858cff6326a71278c1b
Anita Sahu <anisahu002@example.com>
2026-09-04 16:33:49 +0530

    Connect frontend to production backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 
-const API_URL = "http://127.0.0.1:8000";
-const WS_URL = "ws://127.0.0.1:8000/ws";
+const API_URL = "https://real-time-notification-system-1-c4jm.onrender.com";
+const WS_URL = "wss://real-time-notification-system-1-c4jm.onrender.com/ws";
 
 function getToken() {
     return localStorage.getItem("access_token");
